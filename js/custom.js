8c1529ac83e90b69068408b117180d42b43846bb
ahmad-musa <ahmadmusa0987@gmail.com>
2021-09-02 00:12:50 +0600

    3rd (about) section ok

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,29 @@
+$(document).ready(function(){       
+    var scroll_start = 0;
+    var startchange = $('#home');
+    var offset = startchange.offset();
+     if (startchange.length){
+    $(document).scroll(function() { 
+       scroll_start = $(this).scrollTop();
+       if(scroll_start > offset.top) {
+           $(".fixed-top").css('background-color', '#709dca');
+        } else {
+           $('.fixed-top').css('background-color', 'transparent');
+        }
+    });
+     }
+ });
+
+
+// $(function () {
+//     $(document).scroll(function () {
+//       var $nav = $(".navbar-fixed-top");
+//       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+//   });
+  
+
+
 $('.owl-carousel').owlCarousel({
   loop:true,
   margin:10,
